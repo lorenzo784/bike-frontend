9877827b40bab41d9efea9bae1011b738200c770
lorenzo784 <lorenzolpz784@gmail.com>
2025-03-30 11:39:44 -0600

    chore(heroui): add ui library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import {heroui} from "@heroui/react";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,9 +7,11 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './src/**/*.{js,ts,jsx,tsx}',
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()]
 }
